2e7e289755d16c2b444dce0538b6c2e03ba39e42
Ace572 <65081306+Ace572@users.noreply.github.com>
2021-09-13 09:16:56 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,58 @@
+const cvv = document.querySelector('.cvv-container');
+const cvvInput = document.querySelector('.cvv')
+const frontCard = document.querySelector('.front');
+const backCard = document.querySelector('.back');
+const cardNO = document.querySelector('.cardno');
+const frontCardNo = document.querySelector('.frontCardNO');
+const userCvv = document.querySelector('.user-cvv');
+const cardName = document.querySelector('.card-name');
+const userName = document.querySelector('.user-card-name')
+const expiryMonth = document.querySelector('.expiry-month');
+const expiryYear = document.querySelector('.expiry-year');
+const userExpiryMonth = document.querySelector('.user-expiry-month');
+const userExpiryYear = document.querySelector('.user-expiry-year');
+
+function Values(input,output,num){
+    if(input.value.length < num){
+        output.innerText = input.value;
+    }else{
+        alert(`number cannot be more than ${num}`);
+        return;
+    }
+}
+
+expiryYear.addEventListener('keyup',function(){
+    Values(expiryYear,userExpiryYear,3);
+});
+
+
+expiryMonth.addEventListener('keyup',function(){
+    Values(expiryMonth,userExpiryMonth,3);
+});
+
+
+cardNO.addEventListener('keyup', function(){
+    Values(cardNO,frontCardNo,25);
+});
+
+cardName.addEventListener('keyup',function(){
+    Values(cardName,userName,50)
+})
+
+cvvInput.addEventListener('keyup',function(){
+    Values(cvvInput,userCvv,4)
+});
+
+
+
+cvvInput.addEventListener('focus',function(){
+    frontCard.style.animation = "front 0.4s linear forwards";
+    backCard.style.animation = 'back 0.4s linear 0.4s forwards';
+})
+cvvInput.addEventListener('blur',function(){
+    backCard.style.animation = 'reverseBack 0.4s linear forwards';
+    frontCard.style.visibility = 'hidden';
+    frontCard.style.animation = 'reverseFront 0.4s linear 0.4s forwards'
+})
+
+
